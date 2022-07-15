b0ffe8fc84287822aa372ef44c0c03212953a50f
Miguel Brito <miguelfelipe_steam@outlook.com>
2022-07-15 01:07:07 -0300

    Simple Create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Services;
+using SalesWebMvc.Models;
+
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
@@ -17,6 +19,18 @@ namespace SalesWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost] //indicando que a ação abaixo será POST
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index)); //nameof facilita na manutenção
         }
     }
 }
